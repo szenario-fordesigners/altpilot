b98d9771e76394b89868a1910feec220d9a49eb8
Simon Wesp <s.wesp@gmx.net>
2025-11-12 12:15:02 +0100

    explicitly  set model image detail to auto

--- src/services/OpenAiService.php
+++ src/services/OpenAiService.php
@@ -93,7 +93,7 @@ class OpenAiService extends Component
                         'type' => 'image_url',
                         'image_url' => [
                             'url' => $imageData,
-                            'detail' => 'low',
+                            'detail' => 'auto',
                         ],
                     ],
                 ],
