d59e6956e2d9daed0638fd5d1ad34db586fce9d8
Simon Wesp <s.wesp@gmx.net>
2025-11-19 16:18:51 +0100

    add generateOnUpload and generatePerLanguage settings

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -15,4 +15,8 @@ class Settings extends Model
 
     public $openAiPrompt = 'Describe the image provided, make it suitable for an alt text description (roughly 150 characters maximum). Consider transparency within the image if supported by the file type, e.g. don\'t suggest it has a dark background if it is transparent. Do not add a prefix of any kind (e.g. alt text: AI content) so the value is suitable for the alt text attribute value of the image. When describing a person do not assume their gender. Output in {site.language}';
 
+    public $generateOnUpload = false;
+
+    public $generatePerLanguage = true;
+
 }
