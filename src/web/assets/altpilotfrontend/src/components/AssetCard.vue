524e8f555ae490e3c5d2fbc801ac260e54fca91d
Simon Wesp <s.wesp@gmx.net>
2026-01-07 11:55:25 +0100

    regenerate icon

--- src/web/assets/altpilotfrontend/src/components/AssetCard.vue
+++ src/web/assets/altpilotfrontend/src/components/AssetCard.vue
@@ -86,10 +86,12 @@ const anyGenerationFinished = computed(() => {
     <div class="flex p-3">
       <div v-for="site in sites" :key="site.id" class="mb-4 flex w-full">
         <div class="flex w-full justify-between border-b">
-          <div class="text-ap-periwinkle uppercase">{{ site.language }}</div>
+          <div class="text-ap-periwinkle uppercase">
+            {{ site.language }} ({{ assetStatus[currentAsset.status] }})
+          </div>
           <div class="text-ap-periwinkle">{{ charactersRemaining(site.id) }}</div>
           <button
-            class="rounded-full border px-2 transition-colors"
+            class="rounded-full border p-1 transition-colors"
             :class="{
               'opacity-50': generatingBySite[site.id] || isGenerationActive(site.id),
               'hover:bg-white hover:text-ap-periwinkle':
@@ -98,8 +100,17 @@ const anyGenerationFinished = computed(() => {
             :disabled="generatingBySite[site.id] || isGenerationActive(site.id)"
             @click="generateForSite(site.id)"
           >
-            <span v-if="generatingBySite[site.id] || isGenerationActive(site.id)">generating…</span>
-            <span v-else>generate</span>
+            <svg
+              class="regenerate-icon"
+              :class="{ 'animate-spin': generatingBySite[site.id] || isGenerationActive(site.id) }"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path
+                fill="currentColor"
+                d="m410.168 133.046l-28.958-28.958l82.807-.088l-.034-32L328 72.144V208h32v-79.868l27.541 27.541A152.5 152.5 0 0 1 279.972 416l.056 32a184.5 184.5 0 0 0 130.14-314.954M232.028 104l-.056-32a184.5 184.5 0 0 0-130.14 314.954L130.878 416H48v32h136V312h-32v79.868l-27.541-27.541A152.5 152.5 0 0 1 232.028 104"
+              />
+            </svg>
           </button>
         </div>
         <textarea v-model="altTexts[site.id]" class="w-full font-mono" rows="4" />
@@ -107,3 +118,13 @@ const anyGenerationFinished = computed(() => {
     </div>
   </div>
 </template>
+
+<style scoped>
+.regenerate-icon {
+  width: 1rem;
+  height: 1rem;
+
+  /* mirror the icon */
+  transform: scaleX(-1);
+}
+</style>
